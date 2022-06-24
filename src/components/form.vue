7f188b981eb6f22bf9e901b74af6c8ad58fb7c39
chiakaitu <chiakaitu860914@gmail.com>
2022-06-24 17:59:45 +0800

    2022/06/24 新增按鈕樣式

--- src/components/form.vue
+++ src/components/form.vue
@@ -149,7 +149,7 @@
     <!-- 表單 end -->
 
     <!-- 按鈕 start -->
-    <button v-on:click="addGoogle" :disabled="clickLock ? true : false">
+    <button class="button-80" v-on:click="addGoogle" :disabled="clickLock ? true : false">
       送出
     </button>
     <!-- 按鈕 end -->
@@ -275,5 +275,65 @@ label {
 .wrap {
   display: grid;
   text-align: left;
+}
+
+/* CSS */
+.button-80 {
+  background: #fff;
+  backface-visibility: hidden;
+  border-radius: .375rem;
+  border-style: solid;
+  border-width: .125rem;
+  box-sizing: border-box;
+  color: #212121;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 1.125rem;
+  font-weight: 700;
+  letter-spacing: -.01em;
+  line-height: 1.3;
+  padding: .375rem 1.125rem;
+  position: relative;
+  text-align: left;
+  text-decoration: none;
+  transform: translateZ(0) scale(1);
+  transition: transform .2s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.button-80:not(:disabled):hover {
+  transform: scale(1.05);
+}
+
+.button-80:not(:disabled):hover:active {
+  transform: scale(1.05) translateY(.125rem);
+}
+
+.button-80:focus {
+  outline: 0 solid transparent;
+}
+
+.button-80:focus:before {
+  content: "";
+  left: calc(-1*.375rem);
+  pointer-events: none;
+  position: absolute;
+  top: calc(-1*.375rem);
+  transition: border-radius;
+  user-select: none;
+}
+
+.button-80:focus:not(:focus-visible) {
+  outline: 0 solid transparent;
+}
+
+.button-80:focus:not(:focus-visible):before {
+  border-width: 0;
+}
+
+.button-80:not(:disabled):active {
+  transform: translateY(.125rem);
 }
 </style>
